c0e2cf7cbeb149363d952e9bc2989392c23107f5
bender <kudin.dima@gmail.com>
2026-03-12 18:33:36 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,6 +72,7 @@ export default function LandingPage() {
           ]}
           buttonAnimation="slide-up"
           useInvertedBackground={false}
+          fixedMediaHeight={false}
         />
       </div>
 
